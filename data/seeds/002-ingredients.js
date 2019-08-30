a415cd5fed88bbb9e03c89ec6a76a959202929a2
Sandy <sandy.jernigan@gmail.com>
2019-08-29 22:41:29 -0400

    Ran npx knex seed:run -- ran 5 seed files successful

--- data/seeds/002-ingredients.js
+++ data/seeds/002-ingredients.js
@@ -1,15 +1,15 @@
 
 exports.seed = function(knex, Promise) {
   return knex('ingredients').insert([   
-    { ingredient: 'angel food cake mix' },
-    { ingredient: 'any flavor cake mix' },
-    { ingredient: 'water' },
-    { ingredient: 'frosting' },
-    { ingredient: 'Cream Cheese' },
-    { ingredient: 'Sugar' },
-    { ingredient: 'Vanilla Extract' },
-    { ingredient: 'Eggs' },
-    { ingredient: 'Mini Graham Cracker Crusts' },
-    { ingredient: 'Fruit' }
+    { ingredient: 'angel food cake mix' }, // 1
+    { ingredient: 'any flavor cake mix' }, // 2
+    { ingredient: 'water' }, // 3
+    { ingredient: 'frosting' }, // 4
+    { ingredient: 'Cream Cheese' }, // 5
+    { ingredient: 'Sugar' }, // 6
+    { ingredient: 'Vanilla Extract' }, // 7
+    { ingredient: 'Eggs' }, // 8
+    { ingredient: 'Mini Graham Cracker Crusts' }, // 9
+    { ingredient: 'Fruit' } // 10
   ]);
 };
