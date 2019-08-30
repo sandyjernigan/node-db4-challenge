b849ae97d24139e473f00c7277fedf9346c1bb95
Sandy <sandy.jernigan@gmail.com>
2019-08-29 21:27:21 -0400

    recipes table - id, recipe

--- data/migrations/20190829205557_create-recipes-table.js
+++ data/migrations/20190829205557_create-recipes-table.js
@@ -1,3 +1,4 @@
+//#region - Design the data model for a recipe book application
   // manage recipes
   // manage ingredients
 
@@ -9,9 +10,16 @@
   // required for that ingredient as a floating number.
 
   // have a way to save step by step instructions for preparing a recipe.
+//#endregion
 
 exports.up = function(knex) {
+  return knex.schema
   // recipes table - id, recipe
+  .createTable('recipes', tbl => {
+    tbl.increments();
+    tbl.string('recipe', 128).notNullable();
+  })
+
   // ingredients table - id, ingredient
   // recipe_ingredients -- recipe_id, ingredient_id, quantity, measurement
   // steps table - id, recipe_id, step, instructions
