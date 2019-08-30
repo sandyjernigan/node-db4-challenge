4c15ef1ed46d5848286b6df71e06b322404421cd
Sandy <sandy.jernigan@gmail.com>
2019-08-29 20:59:36 -0400

    Setup Model placeholders and empty database

---
+++ api/recipe-model.js
@@ -0,0 +1,34 @@
+const db = require('../data/db-config.js');
+
+module.exports = {
+  add,
+  find,
+  findById,
+  update,
+  remove
+};
+
+//#region - CREATE
+
+// function add
+
+//#endregion
+
+//#region - READ
+
+// find
+// findById
+
+//#endregion
+
+//#region - UPDATE
+
+//update
+
+//#endregion
+
+//#region - DELETE
+
+// remove
+
+//#endregion
