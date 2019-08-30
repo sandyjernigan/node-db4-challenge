2a22afd49f21b2085ff90c9c7e28cc12a35d3e3d
Sandy <sandy.jernigan@gmail.com>
2019-08-29 21:54:57 -0400

    setup exports.up and down

--- data/migrations/20190829205557_create-recipes-table.js
+++ data/migrations/20190829205557_create-recipes-table.js
@@ -17,14 +17,56 @@ exports.up = function(knex) {
   // recipes table - id, recipe
   .createTable('recipes', tbl => {
     tbl.increments();
-    tbl.string('recipe', 128).notNullable();
+    tbl.string('recipe').notNullable().unique();
   })
 
   // ingredients table - id, ingredient
+  .createTable('ingredients', tbl => {
+    tbl.increments();
+    tbl.string('ingredient').notNullable().unique();
+  })
+
+  // steps table - id, step, instructions, recipe_id
+  .createTable('steps', tbl => {
+    tbl.increments();
+    tbl.string('step').notNullable();
+    tbl.text('instructions', longtext).notNullable();
+    // foreign key setup using knex
+    tbl.integer('recipe_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('recipes')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
+  })
+
   // recipe_ingredients -- recipe_id, ingredient_id, quantity, measurement
-  // steps table - id, recipe_id, step, instructions
+  .createTable('recipe_ingredients', tbl => {
+    tbl.integer('recipe_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('recipes')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
+    tbl.integer('ingredient_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('ingredients')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
+    tbl.primary(['recipe_id', 'ingredient_id']);
+    tbl.integer('quantity').notNullable();
+    tbl.string('measurement', 128).notNullable();
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists('recipe_ingredients')
+    .dropTableIfExists('steps')
+    .dropTableIfExists('ingredients')
+    .dropTableIfExists('recipes');
 };
