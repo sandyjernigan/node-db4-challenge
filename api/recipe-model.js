c041039114f4bd6b48079c47997c1ec9b664f637
Sandy <sandy.jernigan@gmail.com>
2019-08-29 23:12:16 -0400

    function getInstructions(recipe_id)

--- api/recipe-model.js
+++ api/recipe-model.js
@@ -2,7 +2,8 @@ const db = require('../data/db-config.js');
 
 module.exports = {
   getRecipes,
-  getShoppingList
+  getShoppingList,
+  getInstructions
 };
 
 //#region - CREATE
@@ -25,6 +26,15 @@ function getShoppingList(recipe_id) {
     .join('ingredients', 'ingredients.id', 'ri.ingredient_id')
     .select('recipes.id', 'recipes.recipe', 'ingredients.ingredient', 'ri.quantity')
     .orderBy('ingredients.id')
+    .where({ 'recipe_id': recipe_id });
+}
+
+// getInstructions(recipe_id): should return a list of step by step instructions for preparing a recipe
+function getInstructions(recipe_id) {
+  return db('steps')
+    .join('recipes', 'recipes.id', 'steps.recipe_id')
+    .select('recipes.id', 'recipes.recipe', 'steps.step', 'steps.instructions')
+    .orderBy('steps.step')
     .where({ 'recipe_id': recipe_id });
 }
 
