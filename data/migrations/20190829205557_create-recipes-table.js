1834ebd6ac210b3d01d0646188308864faf6a6a5
Sandy <sandy.jernigan@gmail.com>
2019-08-29 21:24:10 -0400

    Design the data model for a recipe book application

--- data/migrations/20190829205557_create-recipes-table.js
+++ data/migrations/20190829205557_create-recipes-table.js
@@ -1,6 +1,20 @@
+  // manage recipes
+  // manage ingredients
+
+  // a recipe could have more than one ingredient and 
+  // the same ingredient can be used in multiple recipes. 
+  // Examples are "cup of corn flour" or "gram of butter"
+
+  // when saving the ingredients for a recipe capture the quantity 
+  // required for that ingredient as a floating number.
+
+  // have a way to save step by step instructions for preparing a recipe.
 
 exports.up = function(knex) {
-  
+  // recipes table - id, recipe
+  // ingredients table - id, ingredient
+  // recipe_ingredients -- recipe_id, ingredient_id, quantity, measurement
+  // steps table - id, recipe_id, step, instructions
 };
 
 exports.down = function(knex) {
